a9cfc1b1ce523fe343ec8c16442a7acd3bb2859e
Simon Törnqvist <simon.tornqvist@liu.se>
2024-01-22 16:50:07 +0100

    build: fix babel ts transpilation

--- app/src/renderer/App.tsx
+++ app/src/renderer/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-    return (
-        <div>
-            <h1>Hello from React!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      <MyComponent color="red"/>
+    </div>
+  );
+}
+
+type MyComponentProps = {
+  color?: string;
+};
+
+function MyComponent(props: MyComponentProps) {
+  return <p style={{color: props.color}}>Hello World</p>;
 }
 
 export default App;
